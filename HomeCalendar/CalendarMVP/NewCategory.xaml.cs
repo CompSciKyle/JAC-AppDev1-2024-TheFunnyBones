5b1c4a9e27d0d0408a8ccab5a35c21119427d8be
Rayan-Faccio <rayanfaccio@gmail.com>
2024-04-17 12:29:59 -0400

    Added a closing confirmation window with basic wpf. Added some functionality to the NewCategory class and window. Added the MonthView Button functionalities

--- HomeCalendar/CalendarMVP/NewCategory.xaml.cs
+++ HomeCalendar/CalendarMVP/NewCategory.xaml.cs
@@ -17,7 +17,7 @@ namespace CalendarMVP
     /// <summary>
     /// Interaction logic for NewCategory.xaml
     /// </summary>
-    public partial class NewCategory : Window
+    public partial class NewCategory : Window, ViewInterfaceForEventsAndCategories
     {
         public NewCategory()
         {
@@ -28,6 +28,50 @@ namespace CalendarMVP
         {
             Txb_Description.Text = string.Empty;
         }
+        private void BtnClickCancel(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
 
+        private void BtnClickSave(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+        public void DisplayDB()
+        {
+            TextBlock dbNameTxtBlock = this.Txb_DBName;
+            dbNameTxtBlock.Text = string.Empty; // Get the name of the db and assign it to the Text value of the TextBlock
+            throw new NotImplementedException();
+        }
+        public void DisplayMessage(string message)
+        {
+            throw new NotImplementedException();
+        }
+        public void ClosingConfirmation()
+        {
+            string messageBoxText = "Are you sure you would like to exit the window? Any unsaved changes will be lost."; // Create a class that makes this code a static method so anywhere that needs to use it will have access to it. 
+            string messageBoxCaption = "Exit Window?";
+            MessageBoxButton messageBoxButton = MessageBoxButton.YesNo;
+            MessageBoxImage messageBoxImage = MessageBoxImage.Exclamation;
+            MessageBoxResult result;
+            result = MessageBox.Show(messageBoxText, messageBoxCaption, messageBoxButton, messageBoxImage);
+            if (result == MessageBoxResult.Yes)
+            {
+
+            }
+            else
+            {
+
+            }
+            throw new NotImplementedException();
+        }
+        public void ShowTypes()
+        {
+            throw new NotImplementedException();
+        }
+        public void ShowCategory()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
