5104ab1c0833e84cf4fe78964942188d5b42662a
David Lynch <2275556@johnabbottcollege.net>
2024-05-01 11:03:43 -0400

    added updating Event view interface

---
+++ HomeCalendar/CalendarMVP/ViewInterfaceForUpdatingEvent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Calendar;
+
+namespace CalendarMVP
+{
+    internal class ViewInterfaceForUpdatingEvent
+    {
+        void DisplayDB();
+        void ShowDbName(string DBName);
+        void DisplayMessage(string message);
+        void ShowTypes(List<Category> allCategories);
+    }
+}
